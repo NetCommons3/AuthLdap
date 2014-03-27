a299361ff05b892ef9201a7f82a5ca86ac26b1fc
topaz2 <topaz2.3333@gmail.com>
2014-03-27 15:51:22 +0900

    Added getAuthenticator()

--- Controller/AuthLdapAppController.php
+++ Controller/AuthLdapAppController.php
@@ -3,4 +3,14 @@
 App::uses('AuthController', 'Auth.Controller');
 
 class AuthLdapAppController extends AuthController {
+
+/**
+ * Return authentication adapter name
+ *
+ * @return string Authentication adapter name
+ * @author Jun Nishikawa <topaz2@m0n0m0n0.com>
+ **/
+	public function getAuthenticator() {
+		return 'Ldap';
+	}
 }

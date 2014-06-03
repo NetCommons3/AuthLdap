ff90792a3ca9bc1a1e7341b95bb89156e4c946ef
topaz2 <topaz2.3333@gmail.com>
2014-06-03 18:42:47 +0900

    Fixed phpmd error

--- Test/Case/AllAuthLdapTest.php
+++ Test/Case/AllAuthLdapTest.php
@@ -1,6 +1,11 @@
 <?php
 class AllAuthLdapTest extends CakeTestSuite {
 
+/**
+ * All test suite
+ *
+ * @author Jun Nishikawa <topaz2@m0n0m0n0.com>
+ */
 	public static function suite() {
 		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
 		$suite = new CakeTestSuite(sprintf('All %s Plugin tests', $plugin));

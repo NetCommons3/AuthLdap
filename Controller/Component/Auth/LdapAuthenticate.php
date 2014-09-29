d2ec9b669241c99d9f19272d82cdebeef15fcb34
topaz2 <topaz2.3333@gmail.com>
2014-09-29 11:14:39 +0900

    Fixed doc comments

--- Controller/Component/Auth/LdapAuthenticate.php
+++ Controller/Component/Auth/LdapAuthenticate.php
@@ -3,16 +3,15 @@ App::uses('BaseAuthenticate', 'Controller/Component/Auth');
 /**
  * Auth Controller
  *
- * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
- * @link     http://www.netcommons.org NetCommons Project
- * @license  http://www.netcommons.org/license.txt NetCommons License
+ * @author Jun Nishikawa <topaz2@m0n0m0n0.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
  */
 class LdapAuthenticate extends BaseAuthenticate {
 
 /**
  * __construct
  *
- * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
  * @return   void
  **/
 	public function __construct() {
@@ -22,11 +21,10 @@ class LdapAuthenticate extends BaseAuthenticate {
 /**
  * Checks the fields to ensure they are supplied.
  *
- * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
- * @param    CakeRequest $request The request that contains login information.
- * @param    string $model The model used for login verification.
- * @param    array $fields The fields to be checked.
- * @return   bool False if the fields have not been supplied. True if they exist.
+ * @param CakeRequest $request The request that contains login information.
+ * @param string $model The model used for login verification.
+ * @param array $fields The fields to be checked.
+ * @return bool False if the fields have not been supplied. True if they exist.
  */
 	protected function _checkFields(CakeRequest $request, $model, $fields) {
 		if (empty($request->data[$model])) {
@@ -46,10 +44,9 @@ class LdapAuthenticate extends BaseAuthenticate {
  * to find POST data that is used to find a matching record in the `settings.userModel`. Will return false if
  * there is no post data, either username or password is missing, or if the scope conditions have not been met.
  *
- * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
- * @param    CakeRequest $request The request that contains login information.
- * @param    CakeResponse $response Unused response object.
- * @return   mixed False on login failure. An array of User data on success.
+ * @param CakeRequest $request The request that contains login information.
+ * @param CakeResponse $response Unused response object.
+ * @return mixed False on login failure. An array of User data on success.
  */
 	public function authenticate(CakeRequest $request, CakeResponse $response) {
 		CakeLog::debug('ldap authenticate()');
